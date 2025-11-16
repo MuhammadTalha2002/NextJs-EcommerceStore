332c547d2bfc0a5f14d4fd9fadc88a9482719095
Muhammad Talha <talhazulfiqa1234@gmail.com>
2025-11-16 23:41:30 +0500

    completelyNewInTsx

--- store-front/src/components/Footer.js
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-    return (
-      <footer className="w-full p-4 text-center text-sm border-t bg-white" style={{ color: "black" }}>
-        © {new Date().getFullYear()} ShopEase. All rights reserved.
-      </footer>
-    );
-  }
